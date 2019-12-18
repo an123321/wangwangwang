0d91a02142701d004e062de40a47d4fcb1824518
an123321 <ankai_666@163.com>
2019-12-18 18:15:45 +0800

    在cinema页面增添一个二级路由

--- src/router/cinema/index.js
+++ src/router/cinema/index.js
@@ -1,4 +1,22 @@
 export default {
   path: '/cinema',
-  component: () => import('@/views/Cinema')
+  component: () => import('@/views/Cinema'),
+  children: [
+    {
+      path: "city",
+      component: () => import('@/components/City')
+    },
+    {
+      path: "nowPlaying",
+      component: () => import('@/components/NowPlaying')
+    },
+    {
+      path: "comingSoon",
+      component: () => import('@/components/ComingSoon')
+    },
+    {
+      path: "search",
+      component: () => import('@/components/Search')
+    },
+  ]
 }
