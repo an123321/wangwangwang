3466c9c61f95f9aaf7a20b45c907be801aad5eb4
an123321 <ankai_666@163.com>
2019-12-20 11:11:27 +0800

    配错二级路由，并删除

--- src/router/cinema/index.js
+++ src/router/cinema/index.js
@@ -1,22 +1,4 @@
 export default {
   path: '/cinema',
-  component: () => import('@/views/Cinema'),
-  children: [
-    {
-      path: "city",
-      component: () => import('@/components/City')
-    },
-    {
-      path: "nowPlaying",
-      component: () => import('@/components/NowPlaying')
-    },
-    {
-      path: "comingSoon",
-      component: () => import('@/components/ComingSoon')
-    },
-    {
-      path: "search",
-      component: () => import('@/components/Search')
-    },
-  ]
+  component: () => import('@/views/Cinema')
 }
