d3f5137d3aced8907f3473e5085fb3d5187ae5c4
an123321 <ankai_666@163.com>
2019-12-20 11:13:55 +0800

    新增movie的中间部分

--- src/views/Movie/index.vue
+++ src/views/Movie/index.vue
@@ -1,6 +1,24 @@
 <template>
   <div id="main">
     <Header title="汪汪电影" />
+    <div id="content">
+      <div class="movie_menu">
+        <div class="city_name">
+          <span>大连</span>
+          <i class="iconfont icon-lower-triangle"></i>
+        </div>
+        <div class="hot_swtich">
+          <div class="hot_item active">正在热映</div>
+          <div class="hot_item">即将上映</div>
+        </div>
+        <div class="search_entry">
+          <i class="iconfont icon-sousuo"></i>
+        </div>
+      </div>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </div>
     <TabBar />
   </div>
 </template>
@@ -18,5 +36,55 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+#content .movie_menu {
+  width: 100%;
+  height: 45px;
+  border-bottom: 1px solid #e6e6e6;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: white;
+  z-index: 10;
+}
+.movie_menu .city_name {
+  margin-left: 20px;
+  height: 100%;
+  line-height: 45px;
+}
+.movie_menu .city_name.active {
+  color: #eb9f9f;
+  border-bottom: 2px #eb9f9f solid;
+}
+.movie_menu .hot_swtich {
+  display: flex;
+  height: 100%;
+  line-height: 45px;
+}
+.movie_menu .hot_item {
+  font-size: 15px;
+  color: #666;
+  width: 80px;
+  text-align: center;
+  margin: 0 12px;
+  font-weight: 700;
+}
+.movie_menu .hot_item.active {
+  color: #eb9f9f;
+  border-bottom: 2px #eb9f9f solid;
+}
+.movie_menu .search_entry {
+  margin-right: 20px;
+  height: 100%;
+  line-height: 45px;
+}
+.movie_menu .search_entry.active {
+  color: #eb9f9f;
+  border-bottom: 2px #eb9f9f solid;
+}
+.movie_menu .search_entry i {
+  font-size: 24px;
+  color: #eb9f9f;
+}
 </style>
+
