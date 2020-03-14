1f741144d4de2da1a7b7471c44a75dae7ca80985
an123321 <ankai_666@163.com>
2020-03-14 10:55:13 +0800

    "Cinema"影院组件增加了一个二级路由

--- src/views/Cinema/index.vue
+++ src/views/Cinema/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="main">
     <Header title="汪汪影院" />
+    <div id="content">
+      <div class="cinema_menu">
+        <div class="city-switch">
+          全球
+          <i class="iconfont icon-lower-triangle"></i>
+        </div>
+        <div class="city-switch">
+          品牌
+          <i class="iconfont icon-lower-triangle"></i>
+        </div>
+        <div class="city-switch">
+          特色
+          <i class="iconfont icon-lower-triangle"></i>
+        </div>
+      </div>
+      <CiList />
+    </div>
     <TabBar />
   </div>
 </template>
@@ -8,15 +25,27 @@
 <script>
 import Header from "@/components/Header";
 import TabBar from "@/components/TabBar";
+import Cilist from "@/components/CiList";
 
 export default {
   name: "Cinema",
   components: {
     Header,
-    TabBar
+    TabBar,
+    Cilist
   }
 };
 </script>
 
-<style>
+<style scoped>
+#content .cinema_menu {
+  width: 100%;
+  height: 45px;
+  border-bottom: 1px solid #e6e6e6;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background: white;
+}
 </style>
+
