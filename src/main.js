f7629a950c2596332d313d0af3ba29a011210c75
an123321 <ankai_666@163.com>
2020-03-23 21:58:46 +0800

    main.js"增加了axios"

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import axios from 'axios'
+Vue.prototype.axios = axios;
+
 Vue.config.productionTip = false
 
 new Vue({
